9065647eb451813ac4acb1f8972b9b17a7e11d2a
vivanan <77786158@qq.com>
2016-08-18 23:13:09 +0800

    新增个人主页，把原主页改为index1.html

---
+++ href/more/fishgame/js/web.js
@@ -0,0 +1,15 @@
+function Web(){
+    this.x=0;
+    this.y=0;
+    this.scale=0.5;
+}
+Web.prototype.draw=function(gd){
+    gd.save();
+    gd.translate(this.x, this.y);
+    gd.scale(this.scale, this.scale);
+    gd.drawImage(JSON['web'],
+        22,23,200,200,
+        -100,-100,200,200
+    );
+    gd.restore();
+};
